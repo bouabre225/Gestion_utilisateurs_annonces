d038f0634f0d89706f2014b593dbb2a151c1d3b0
Ange KORE <angebouabre463@gmail.com>
2025-06-08 23:57:50 +0100

    correction

--- Gestion des utilisateurs et des annonces/index.php
+++ Gestion des utilisateurs et des annonces/index.php
@@ -7,7 +7,7 @@ include("./includes/head.php");?>
                         <div class="card p-4 text-center">
                             <i class="bi bi-people-fill display-5 text-primary mb-2"></i>
                             <h5 class="card-title">Utilisateurs</h5>
-                            <p class="mb-0"><a href="./users.php" class='btn btn-primary btn-sm'>Gérez vos utilisateurs facilement.</a></p>
+                            <p class="mb-0"><a href="./users.php" class='btn btn-primary btn-sm'>Gérez vos utilisateurs facilement</a></p>
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -15,13 +15,13 @@ include("./includes/head.php");?>
                             <i class="bi bi-bar-chart-fill display-5 text-success mb-2"></i>
                             <h5 class="card-title">Annonces</h5>
                             <p class="mb-0"><a href="./annonces.php" class='btn btn-primary btn-sm'>voir les differentes annonces</a></p>
-                        </div>
+                        </div>  
                     </div>
                     <div class="col-md-4">
                         <div class="card p-4 text-center">
                             <i class="bi bi-gear-fill display-5 text-warning mb-2"></i>
                             <h5 class="card-title">Paramètres</h5>
-                            <p class="mb-0"><a href="./parametre.php" class='btn btn-primary btn-sm'>Personnalisez votre profil.</a></p>
+                            <p class="mb-0"><a href="./parametre.php" class='btn btn-primary btn-sm'>Personnalisez votre profil</a></p>
                         </div>
                     </div>
                 </div>
